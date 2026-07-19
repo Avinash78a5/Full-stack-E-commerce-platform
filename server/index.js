d11778fc08388b77b23281369d1c3356d5aa1ceb
Avinash82236 <avinashsr273@gmail.com>
2026-07-19 09:50:16 +0530

    modified the client url in backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/payments', paymentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.CLIENT_URL
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
