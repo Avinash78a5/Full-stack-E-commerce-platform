d418baf5d786741192a178efff8085b8c9178e4f
Avinash82236 <avinashsr273@gmail.com>
2026-07-19 19:58:52 +0530

    enabled frontend url in cors to allow one origin only

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ import paymentRoutes from './routes/paymentRoutes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }
+));
 app.use('/api/payments', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
