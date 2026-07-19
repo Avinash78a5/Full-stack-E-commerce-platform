b90ac4faafda9d563ef8fe542ee12c6a075b4362
Avinash82236 <avinashsr273@gmail.com>
2026-07-19 16:21:02 +0530

    Fix Render PORT configuration

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/payments', paymentRoutes);
 
-const PORT = process.env.CLIENT_URL
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
